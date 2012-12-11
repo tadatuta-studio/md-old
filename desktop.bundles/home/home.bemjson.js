3d33d90b2100ad3f2c3143674f57c3b65ef9f1bf
m-peshekhonov <m-peshekhonov@tdtt-mac.local>
2012-12-12 00:03:51 +0200

    home page start

---
+++ desktop.bundles/home/home.bemjson.js
@@ -0,0 +1,132 @@
+({
+    block: 'b-page',
+    title: 'Magic Desktop 8',
+    favicon: '/favicon.ico',
+    head: [
+        { elem: 'css', url: '../merged/_merged.css', ie: false },
+        { elem: 'css', url: '../merged/_merged', ie: true },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: '../merged/_merged.js' },
+        { elem: 'meta', attrs: { name: 'description', content: '' }},
+        { elem: 'meta', attrs: { name: 'keywords', content: '' }}
+    ],
+    mods: { page: 'home' },
+    content:[
+        {
+            block: 'header',
+            mods: { page: 'home' }
+        },
+        {
+            elem: 'inner',
+            mix: [{ block: 'clearfix' }],
+            content: [
+                {
+                    block: 'chapter',
+                    mix: [{ block: 'clearfix' }],
+                    content: [
+                        {
+                            elem: 'inner',
+                            content: [
+                                {
+                                    elem: 'title',
+                                    content: 'Partner with us!'
+                                },
+                                {
+                                    elem: 'description',
+                                    content: 'We have many different types of affiliate & partner programs that we offer, if you are a software reseller, website owner, ISP, OEM or a private person that wants to sell Magic Desktop, we have the right program for you!'
+                                },
+                                {
+                                    block: 'button',
+                                    mods: { size: '240', theme: 'blue', type: 'apply' },
+                                    content: 'Apply now!'
+                                }
+                            ]
+                        },
+                        {
+                            block: 'units',
+                            content: [
+                                {
+                                    elem: 'notice',
+                                    content: 'Who are you?'
+                                },
+                                {
+                                    elem: 'inner',
+                                    content: [
+                                        {
+                                            elem: 'item',
+                                            content: [
+                                                {
+                                                    elem: 'title',
+                                                    content: 'Software reseller'
+                                                },
+                                                {
+                                                    elem: 'description',
+                                                    content: 'You are a software reseller and have your own online store, start making money today by selling Magic Desktop to your customers.'
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            elem: 'item',
+                                            content: [
+                                                {
+                                                    elem: 'title',
+                                                    content: 'Software reseller'
+                                                },
+                                                {
+                                                    elem: 'description',
+                                                    content: 'You are a software reseller and have your own online store, start making money today by selling Magic Desktop to your customers.'
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            elem: 'item',
+                                            content: [
+                                                {
+                                                    elem: 'title',
+                                                    content: 'Software reseller'
+                                                },
+                                                {
+                                                    elem: 'description',
+                                                    content: 'You are a software reseller and have your own online store, start making money today by selling Magic Desktop to your customers.'
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            elem: 'item',
+                                            content: [
+                                                {
+                                                    elem: 'title',
+                                                    content: 'Software reseller'
+                                                },
+                                                {
+                                                    elem: 'description',
+                                                    content: 'You are a software reseller and have your own online store, start making money today by selling Magic Desktop to your customers.'
+                                                }
+                                            ]
+                                        },
+                                        {
+                                            elem: 'item',
+                                            content: [
+                                                {
+                                                    elem: 'title',
+                                                    content: 'Software reseller'
+                                                },
+                                                {
+                                                    elem: 'description',
+                                                    content: 'You are a software reseller and have your own online store, start making money today by selling Magic Desktop to your customers.'
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            block: 'footer'
+        }
+    ]
+})
