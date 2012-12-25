47cb33801db7618930b737a27b16414e8f9b1b5d
Vladimir Grinenko <i@tadatuta.com>
2012-12-25 09:02:31 +0200

    dropdown.js from master

--- desktop.blocks/dropdown/dropdown.js
+++ desktop.blocks/dropdown/dropdown.js
@@ -1,7 +1,24 @@
 BEM.DOM.decl('dropdown',
 {
+    onSetMod: {
+        'visibility': {
+            'visible': function() {
+                this.afterCurrentEvent(function() {
+                    this.bindToDoc('click', this.hide);
+                });
+            },
+            '': function() {
+                this.unbindFromDoc('click', this.hide);
+            }
+        }
+    },
     toggle: function() {
         this.toggleMod('visibility', 'visible');
+
+        return this;
+    },
+    hide: function() {
+        this.delMod('visibility');
 
         return this;
     }
